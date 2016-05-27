31d5b54a4bfdd444c08b831c27ce69c890384d41
CJGitH <2392777628@qq.com>
2016-05-27 22:16:17 +0800

    测试配置

---
+++ Campus-Commander/ViewController.m
@@ -0,0 +1,28 @@
+//
+//  ViewController.m
+//  Campus-Commander
+//
+//  Created by chen on 16/5/27.
+//  Copyright © 2016年 chen. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    
+    NSLog(@"测试");
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
